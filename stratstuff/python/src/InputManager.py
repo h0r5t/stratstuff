d69110fdae1dae81d7d89ef33192309d47f1e778
h0r5t <fabianharfst@gmail.com>
2015-01-09 11:38:48 +0100

    Update InputManager.py

--- stratstuff/python/src/InputManager.py
+++ stratstuff/python/src/InputManager.py
@@ -1,1 +1,21 @@
 class InputManager:
+  def __init__(self):
+    #do some init stuff
+  
+  def handleInput(self, message):
+    #user entered some command via the engine interface, handle these
+    
+    #--------------- here it comes --------------------
+    
+    split = message.split(" ")
+    selection = None
+    inputIDString = split[0]
+    
+    if split.length > 1:
+      #we have some selection data that specifies where to do the task
+      selection = split[1]
+    
+    if inputIDString == "input::chopTrees"
+      #do some stuff for the selection (some x,y,z bounds for where to do cuttrees...
+    
+    #--------------- here it comes --------------------
