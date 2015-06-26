eb0781702f05c54ce9e8de6617db2b7a8cce3f47
h0r5t <gg>
2015-06-26 22:12:51 +0200

    test commit from atom

--- stratstuff/python/src/custom/TestRobot2.py
+++ stratstuff/python/src/custom/TestRobot2.py
@@ -1,3 +1,4 @@
+
 from random import randint
 
 from src.API.Mode import Mode
@@ -5,17 +6,16 @@ from src.API.Robot import Robot
 from src.API.State import State
 
 
-
 class TestRobot2(Robot):
     def __init__(self, adapter, m_objectID):
         Robot.__init__(self, adapter, m_objectID)
-        
+
         self.testmode = TestMode(None, self)
         self.setMode(0, self.testmode)
-        
+
     def signalReceived(self, message):
         print(message)
-    
+
 class TestMode(Mode):
     def __init__(self, adapter, robot):
         Mode.__init__(self, adapter, robot)
@@ -23,11 +23,11 @@ class TestMode(Mode):
         self.setState(0, teststate)
         teststate = MoveState2(adapter, robot, self)
         self.setState(1, teststate)
-        
+
 class MoveState1(State):
     def __init__(self, adapter, robot, mode):
         State.__init__(self, adapter, robot, mode)
-        
+
     def execute(self):
         self.robot.moveTo(10, 10, 0)
         x = randint(0, 20)
@@ -35,12 +35,12 @@ class MoveState1(State):
         self.robot.turn(x, y, 0)
         for x in range(0, 5):
             self.robot.fire()
-        
-        
+
+
 class MoveState2(State):
     def __init__(self, adapter, robot, mode):
         State.__init__(self, adapter, robot, mode)
-        
+
     def execute(self):
         self.robot.moveTo(10, 25, 0)
         x = randint(0, 30)
@@ -48,19 +48,17 @@ class MoveState2(State):
         self.robot.turn(x, y, 0)
         for x in range(0, 5):
             self.robot.fire()
-            
+
 class IdleMode(Mode):
     def __init__(self, adapter, robot):
         Mode.__init__(self, adapter, robot)
         idlestate = IdleState(adapter, robot, self)
         self.setState(0, idlestate)
-            
+
 class IdleState(State):
     def __init__(self, adapter, robot, mode):
         State.__init__(self, adapter, robot, mode)
-        
+
     def execute(self):
         self.robot.wait(5000)
         self.robot.setMode(self.testmode)
-    
-        
