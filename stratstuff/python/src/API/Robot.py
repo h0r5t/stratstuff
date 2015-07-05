f58b75bd350c032a9cf04bf12e3ad413e8edb9be
h0r5t <gg>
2015-07-05 21:24:14 +0200

    commit

--- stratstuff/python/src/API/Robot.py
+++ stratstuff/python/src/API/Robot.py
@@ -8,42 +8,42 @@ class Robot(Thread):
     def __init__(self, adapter, m_objectID):
         Thread.__init__(self)
         self.adapter = adapter
-        self.objectID = m_objectID  
-        self.modes = {}     
+        self.objectID = m_objectID
+        self.modes = {}
         self.currentModeIndex = 0
         self.visionScope = None
-        
+
     def run(self):
         self.execute()
-        
+
     def destroy(self):
         self.modes[self.currentModeIndex].destroyMode()
-    
+
     def update(self):
         pass
-    
+
     def getObjectID(self):
         return self.objectID
-    
+
     def execute(self):
         self.modes[self.currentModeIndex].execute()
-    
+
     def setMode(self, index, mode):
         self.modes[index] = mode
-        
+
     def watchEvent(self, event):
         while 1:
             if event.wasCalledBack():
                 return event.getMessage()
             time.sleep(SLEEP_TIME)
-    
+
     # interface methods for the robot
-    
+
     def signalReceived(self, message):
         pass
-        
+
     # user commands for the robot
-    
+
     def moveTo(self, x, y, z):
         # returns true if unit could successfully move to target, false if not
         event = self.adapter.registerMoveTask(self.objectID, x, y, z)
@@ -51,7 +51,7 @@ class Robot(Thread):
             return
         msg = self.watchEvent(event)
         return bool(msg)
-    
+
     def getScope(self):
         # returns everything the robot can see as a Scope object
         event = self.adapter.registerGetScope(self.objectID)
@@ -60,7 +60,7 @@ class Robot(Thread):
         msg = self.watchEvent(event)
         scope = Scope(msg)
         return scope
-    
+
     def turn(self, x, y, z):
         # turns the roboter until he faces the target point, returns true if successful
         event = self.adapter.registerObjectTurn(self.objectID, x, y, z)
@@ -68,12 +68,12 @@ class Robot(Thread):
             return
         msg = self.watchEvent(event)
         return bool(msg)
-    
+
     def fire(self):
         # robot fires in front
         self.adapter.registerFire(self.objectID)
         self.wait(50)
-        
+
     def wait(self, millis):
         # waits the amount of millis and calls back after
         event = self.adapter.registerIdleTask(self.objectID, millis)
@@ -81,7 +81,7 @@ class Robot(Thread):
             return
         msg = self.watchEvent(event)
         return msg
-    
+
     def mine(self, x, y, z):
         # mines the wp
         self.turn(x, y, z)
@@ -90,10 +90,9 @@ class Robot(Thread):
             return
         msg = self.watchEvent(event)
         return bool(msg)
-    
+
     def sendRadialSignal(self, message):
         # sends a signal in all directions with the given message
         self.wait(500)
         self.adapter.registerRadialSignal(self.objectID, message)
         self.wait(500)
-    
